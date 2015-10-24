d12405870c41504ef3aa2d17181476feefdab98c
Michael Colburn <mac002@theColburns.us>
2015-10-24 15:47:34 +0300

    Additional constants.  Not properly used yet.

--- net.ages.liturgical.workbench.transformer/src/net/ages/liturgical/workbench/transformer/epub/Constants.java
+++ net.ages.liturgical.workbench.transformer/src/net/ages/liturgical/workbench/transformer/epub/Constants.java
@@ -1,7 +1,9 @@
 package net.ages.liturgical.workbench.transformer.epub;
 
 public class Constants {
-	public static final String EPUB_SERVICE_DIR = "e/s/";
+	public static final String EPUB_DIR = "e/";
+	public static final String EPUB_SERVICE_DIR = EPUB_DIR + "s/";
+	public static final String EPUB_MERGED_DIR = EPUB_DIR + "m/";
 	// the number of ../ must match the count of the forward slashes in EPUB_SERVICE_DIR
 	public static final String EPUB_INDEX_BASE_REF = "<base href='../../'/>";
 	public static final String PATH_TO_RESOURCES = "resources/";
